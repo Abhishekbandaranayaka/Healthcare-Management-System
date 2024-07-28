100b3d9640cbcb2b2a10e4e6b631cde8026e0921
Ima Herath <imaherath13@gmail.com>
2024-07-28 04:30:10 -0700

    Appoinment Service

---
+++ appointment-service/src/main/java/com/healthcare_app/appointment_service/AppointmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare_app.appointment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentServiceApplication.class, args);
+	}
+
+}
