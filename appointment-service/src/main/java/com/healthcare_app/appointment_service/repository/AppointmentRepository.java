0e851ac649ed3344768955cc7ce0099c42c65299
Ima Herath <imaherath13@gmail.com>
2024-08-09 13:30:38 +0530

    Updated Appoinment Service

--- appointment-service/src/main/java/com/healthcare_app/appointment_service/repository/AppointmentRepository.java
+++ appointment-service/src/main/java/com/healthcare_app/appointment_service/repository/AppointmentRepository.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Repository interface for managing Appointment entities.
  * Extends JpaRepository to provide CRUD operations and query methods for Appointment entities.
  *
- * Author: [Your Name]
- * Date: 2024/08/09
+ * Author: Ima Herath
  */
 
 public interface AppointmentRepository extends JpaRepository<Appointment , Long> {
