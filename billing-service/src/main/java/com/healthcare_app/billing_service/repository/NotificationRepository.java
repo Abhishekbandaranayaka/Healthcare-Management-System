832d2c75d311e50598da4526cc8821c452491dbf
Abhishek Rathnayaka <89740367+Abhishekbandaranayaka@users.noreply.github.com>
2024-09-05 01:29:16 +0530

    remove-notification

--- billing-service/src/main/java/com/healthcare_app/billing_service/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.healthcare_app.billing_service.repository;
-
-import com.healthcare_app.billing_service.model.Notification;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NotificationRepository extends MongoRepository<Notification , String> {
-}
